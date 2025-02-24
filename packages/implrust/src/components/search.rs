4dddf6c89a660b97bf67fa826ab6b506e05b5b47
ImplFerris <implferris@proton.me>
2025-02-24 08:09:41 +0530

    bug fix

--- packages/implrust/src/components/search.rs
+++ packages/implrust/src/components/search.rs
@@ -4,7 +4,7 @@ pub fn generate_search_index() {
         use crate::{static_dir, Route};
 
         std::env::set_var("CARGO_MANIFEST_DIR", static_dir().join("assets"));
-        let version_filter: [(&str, fn(&Route) -> bool); 4] =
+        let version_filter: [(&str, fn(&Route) -> bool); 1] =
             [("0_6", |route| matches!(route, Route::Docs06 { .. }))];
         for (version, filter) in version_filter {
             dioxus_search::SearchIndex::<Route>::create(
